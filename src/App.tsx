cbce2ebf2420206697b38adb274ea62a2a646ca7
gayanDeeegala <gayandeegala@gmail.com>
2023-04-24 23:31:12 +0530

    add mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import './App.css'
+import { Button } from '@mui/material'
 
 function App() {
-  return <>Empty</>
+  return (
+    <>
+      <Button variant='contained'>Contained</Button>
+    </>
+  )
 }
 
 export default App
