e43a81a218f597192253da50693c0c12438c0a78
gayanDeeegala <gayandeegala@gmail.com>
2023-05-11 22:38:01 +0530

    create useComplexSearch hoook

---
+++ src/hooks/useComplexSearch.ts
@@ -0,0 +1,12 @@
+import { useQuery } from 'react-query';
+import { Filters } from '../modules/Explore/components/types';
+
+export const useComplexSearch = ({ filters }: { filters: Filters }) => {
+  const { type, diet } = filters;
+  return useQuery(['recipes', 'complexSearch', filters], async () => {
+    const res = await fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=3a919f863b6f473e93b2473cdd0b6e3d&type=${type}&diet=${diet}`,
+    );
+    return res.json();
+  });
+};
