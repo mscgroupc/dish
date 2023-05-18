18530979b4f4b5275fd266c0b4b18de9d1c233ec
gayanDeeegala <gayandeegala@gmail.com>
2023-05-18 23:06:12 +0530

    update key

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const apiConfig = {
   spoonacular: {
     baseUrl: 'https://api.spoonacular.com',
-    apiKey: '05a5c68547834e678c41f731f0854604',
+    apiKey: '63c48bc579074b54b6bc1be60924bff6',
   },
 };
